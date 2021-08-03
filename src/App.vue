f0a4a8077f9c661f9df61f403609e7de3f732a44
Seric-y <1490048251@qq.com>
2021-08-03 20:34:15 +0800

    'Login'

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
     <router-view/>
   </div>
 </template>
-
 <style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/Login.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		1111
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
