7a67b45d21534a2faed00ffb618fbd43bec82add
Seric-y <1490048251@qq.com>
2021-08-04 09:39:42 +0800

    项目初始化-调整初始化目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- 路由的出口 -->
     <router-view/>
+		<h1>黑马头条</h1>
   </div>
 </template>
+<script>
+	export default{
+		name:'App'
+	}
+</script>
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入全局样式表
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
