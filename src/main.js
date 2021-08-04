e2609cc6b6b6883051b15aa4e9e939ee92ca09f6
Seric-y <1490048251@qq.com>
2021-08-04 10:34:08 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 加载 Vant 核心组件库
+import Vant from 'vant';
+
+// 加载 Vant 全局样式
+import 'vant/lib/index.css';
+
 // 引入全局样式表
 import './styles/index.less'
+
+// 注册使用 Vant 组件库
+Vue.use(Vant);
 
 Vue.config.productionTip = false
 
